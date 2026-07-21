8f3d9cc6addd8616ecbced9328783395cb304170
Vamsi Esuru <esuruvamshi@gmail.com>
2026-07-21 19:07:34 +0530

    Refactor merging logic and median calculation
    Refactor input handling for merging two sorted arrays and calculate median correctly.

--- merging of 2 sorted arrays.java
+++ merging of 2 sorted arrays.java
@@ -6,21 +6,20 @@ public class Solution {
     public static void main(String[] args) 
     {
        Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int a[] = new int[n];
-       for(int i=0;i<n;i++)
-       {
-            a[i] = sc.nextInt();
-            
-       }
-       Arrays.sort(a);
-       int m = sc.nextInt();
-       int b[]= new int[m];
-       for(int i=0;i<m;i++)
-       {
-            b[i] = sc.nextInt();
-       }
-       Arrays.sort(b);
+     int n = sc.nextInt();
+     int m = sc.nextInt();
+     int[] a = new int[n];
+     for(int i = 0; i < n; i++)
+     {
+        a[i] = sc.nextInt();
+     }
+     int[] b = new int[m];
+     for(int i = 0; i < m; i++)
+     {
+        b[i] = sc.nextInt();
+     }
+     Arrays.sort(a);
+     Arrays.sort(b);
        int i=0,j=0,k=0;
        int r[]=new int[m+n];
        while(i<n && j<m)
@@ -48,15 +47,16 @@ public class Solution {
             k++;
             j++;
         }
-        if(k % 2 == 0)
+        int total =n+m;
+        if(total % 2 == 0)
         {
-            long x = r[k/2 - 1];
-            long y = r[k/2];
+            long x = r[total/2 - 1];
+            long y = r[total/2];
             System.out.printf("%.1f\n",(x+y)/2.0);
         }
         else
         {
-            System.out.printf("%.1f\n",(double)r[k/2]);
+            System.out.printf("%.1f\n",(double)r[total/2]);
         }
         sc.close();    
     }
