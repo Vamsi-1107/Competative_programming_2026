8bc9e8c06e97d839746b9fb07d3f789d8dad3105
Vamsi Esuru <esuruvamshi@gmail.com>
2026-07-21 10:15:33 +0530

    Add Java program to compute the sum of two numbers

---
+++ Addition of 2 numbers.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+    public static int getSum(int a, int b) 
+       {
+        while (b != 0) 
+        {
+            int carry = (a & b) << 1; 
+            a = a ^ b;              
+            b = carry;               
+        }
+        return a;
+       }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(getSum(n,m));
+       
+    }
+}
