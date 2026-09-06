019c0198aad2564d204e11dcb00a43bad0eab0dc
Vamsi Esuru <esuruvamshi@gmail.com>
2026-09-06 14:44:19 +0530

    Refactor duplicate character finder with BufferedReader

--- find duplicate character.java
+++ find duplicate character.java
@@ -1,33 +1,49 @@
 import java.io.*;
-import java.util.*;
 
-public class Solution 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        StringBuilder r = new StringBuilder();
-        int map = 0;
-        int n = s.length();
-        for(int i=0;i<n;i++)
-        {
-            int c = s.charAt(i)-'a';
-            if((map&(1<<c)) != 0)
-            {
-                r.append(s.charAt(i));
-            }
-            else
-            {
-                map = map | (1<<c);
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        if (s == null || s.trim().isEmpty()) {
+            System.out.println("No duplicates");
+            return;
+        }
+
+        s = s.trim();
+
+        int seen = 0;
+        int duplicates = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int bit = 1 << (s.charAt(i) - 'a');
+            if ((seen & bit) != 0) {
+                duplicates |= bit; 
+            } else {
+                seen |= bit;
             }
         }
-        for(int i=0;i<r.length();i++)
-        {
-            System.out.print(r.charAt(i)+ " ");
+
+        int printed = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int bit = 1 << (ch - 'a');
+
+            if ((duplicates & bit) != 0 && (printed & bit) == 0) {
+                if (result.length() > 0) {
+                    result.append(" ");
+                }
+                result.append(ch);
+                printed |= bit; 
+            }
+        }
+
+        if (result.length() > 0) {
+            System.out.println(result.toString());
+        } else {
+            System.out.println("No duplicates");
         }
     }
 }
-        
-        
-
